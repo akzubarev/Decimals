5ab9ca3d2cf816607be32e1af08ed6c475f0890c
Alex <alexkzubarev@gmail.com>
2021-11-05 01:22:34 +0300

    Added versioning to authors activity

--- app/src/main/java/com/education4all/activities/AuthorsActivity.java
+++ app/src/main/java/com/education4all/activities/AuthorsActivity.java
@@ -1,6 +1,7 @@
 package com.education4all.activities;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.education4all.BuildConfig;
 import com.education4all.R;
 
 public class AuthorsActivity extends AppCompatActivity {
@@ -29,8 +31,18 @@ public class AuthorsActivity extends AppCompatActivity {
         ss.setSpan(new URLSpan(getString(R.string.donatelink)), 0, ss.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         donate.setText(ss);
         //  donate.setMovementMethod(LinkMovementMethod.getInstance());
+        try {
+            String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            TextView versionTV = findViewById(R.id.version);
+            String versionTag = "";
+            if (BuildConfig.BUILD_TYPE.equals("debug"))
+                versionTag = " (beta)";
+            String versionText = String.format("Версия: %s%s", versionName, versionTag).trim();
+            versionTV.setText(versionText);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
-
 
     public void goToWeb(View view) {
         Intent intent = new Intent(this, WebActivity.class);
